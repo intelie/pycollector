25ab341b8140e1b8ec6e15e4cf677732eb7ec208
Ronald Kaiser <raios.catodicos@gmail.com>
2011-07-07 11:06:29 -0300

    changing conf to accept groups inside regexp

--- tests/one_event_per_line.py
+++ tests/one_event_per_line.py
@@ -12,19 +12,10 @@ class TestOneEventPerLine(unittest.TestCase):
     def testMostSimpleEvent(self):
         line = 'go there and get me some food.'
         conf = {'events_config' : [{'eventtype' : 'my-wonderful-type',
-                                    'patterns' : [{'regexp' : '.*foo.*'}]}] }
+                                    'regexps' : ['.*foo.*']}]}
         event = create_event(line, conf)
         expected_event = {'eventtype' : 'my-wonderful-type', 'line' : line}
         self.assertDictEqual(expected_event, event)
-
-
-    def testEventWithRegexpGroups(self):
-        line = 'There\'s nothing you can do that can\'t be done'
-        conf = {'events_config' : [{'eventtype' : 'my-wonderful-type',
-                                    'patterns' : [{'regexp' : '.*(you).*(can).*', 
-                                                   'group_names' : ['group1', 'group2']}]}]}
-        event = create_event(line, conf)
-        self.assertDictContainsSubset({'group1' : 'you', 'group2' : 'can'}, event)
 
 
     def testWithUserDefinedAndGlobalConfs(self):
@@ -36,8 +27,7 @@ class TestOneEventPerLine(unittest.TestCase):
            },
            'events_config': [{
                 'eventtype': 'acessos',
-                'patterns': [
-                        {'regexp': '^lex.*$'}],
+                'regexps': ['^lex.*$'],
                 'one_event_per_line_conf': {
                     'user_defined_fields': {
                     'passo': 'passo 1',
@@ -51,30 +41,28 @@ class TestOneEventPerLine(unittest.TestCase):
         self.assertDictContainsSubset(expected_subset, event)
 
 
-    def testWithoutPattern(self):
+    def testWithoutRegexps(self):
         line = 'imagination is more important than knowledge'
         conf = {'events_config' : [{'eventtype' : 'my-wonderful-type'}]}
-        with self.assertRaises('PatternNotFound'):
+        with self.assertRaises('RegexpsNotFound'):
             create_event(line, conf)
 
 
-    def testWithEmptyListOfPatterns(self):
+    def testWithEmptyListOfRegexps(self):
         line = 'go, go, go, marine!'
         conf = {'events_config' : [{'eventtype' : 'my-wonderful-type',
-                                    'patterns' : []}]}
+                                    'regexps' : []}]}
         event = create_event(line, conf)
         expected_event = None
         self.assertDictEqual(expected_event, event)
 
 
-    def testInconsistentNumberOfGroups(self):
-        line = "all you need is love!"
+    def testEventWithRegexpGroups(self):
+        line = 'There\'s nothing you can do that can\'t be done'
         conf = {'events_config' : [{'eventtype' : 'my-wonderful-type',
-                                    'patterns' : [{'regexp' : '^(all).*(need).*(love).*', 
-                                                   'group_names' : ['group1']}]}]}
+                                    'regexps' : ['.*(?P<who>you).*(?P<verb>can).*']}]}
         event = create_event(line, conf)
-        expected_subset = {'group1' : 'all'}
-        self.assertDictContainsSubset(expected_subset, event)
+        self.assertDictContainsSubset({'who' : 'you', 'verb' : 'can'}, event)
 
         
 #TODO: melhorar organizacao dos confs
