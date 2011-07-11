d1f7beafcc2cd40e1e9f7e9a118f837b08bd48a1
Ronald Kaiser <raios.catodicos@gmail.com>
2011-07-11 09:46:07 -0300

    refactoring event creation

--- src/log_lines_manager.py
+++ src/log_lines_manager.py
@@ -17,9 +17,12 @@ class LogLinesManager:
     def __init__(self, conf):
         self.validate_conf(conf)
         self.conf = conf
-        self.counts = {}
         self.init_counts()
+        self.consolidated = []
         self.event_queue = []
+
+    def has_global_fields(self):
+        return self.conf.has_key('global_fields')
 
     def init_counts(self):
         events_conf = self.conf['events_conf']
@@ -39,37 +42,33 @@ class LogLinesManager:
             if not event_conf.has_key('regexps'):   
                 raise RegexpNotFound()
 
-    def process_line(self, line):
+    def create_event(self, line, groups_matched, conf):
         event = {}
-        no_match = True
-        already_match = False
+        event.update({'eventtype' : conf['eventtype']})
+        event.update(groups_matched)
+        if conf.has_key('one_event_per_line_conf') and \
+           conf['one_event_per_line_conf'].has_key('user_defined_fields'):
+            event.update(conf['one_event_per_line_conf']['user_defined_fields'])
+            event.update({'line' : line})
+        elif conf.has_key('consolidation_conf'):
+            if conf['consolidation_conf'].has_key('enable') and \
+                conf['consolidation_conf']['enable'] == False:
+                pass
+            else:
+                self.counts[event_conf['consolidation_conf']['field']] += 1
+        else:
+            event.update({'line' : line})            
+        if self.has_global_fields():
+            event.update(self.conf['global_fields'])
+        return event
+
+    def process_line(self, line):
         events_conf = self.conf['events_conf']
         for event_conf in events_conf:
             regexps = event_conf['regexps']
             for regexp in regexps:
                 match = re.match(regexp, line)
                 if match:
-                    no_match = False
-                    event.update({'eventtype' : event_conf['eventtype']})
-                    event.update({'line' : line})
-                    event.update(match.groupdict())
-                    if event_conf.has_key('one_event_per_line_conf') and \
-                       event_conf['one_event_per_line_conf'].has_key('user_defined_fields'):
-                        event.update(event_conf['one_event_per_line_conf']['user_defined_fields'])
-                    elif event_conf.has_key('consolidation_conf'):
-                        if event_conf['consolidation_conf'].has_key('enable') and \
-                            event_conf['consolidation_conf']['enable'] == False:
-                            pass
-                        else:
-                            self.counts[event_conf['consolidation_conf']['field']] += 1                        
-                    already_match = True
-                    break
-            if already_match:
-                break
-
-        if no_match:
-            return 
-        elif 'global_fields' in self.conf:
-            event.update(self.conf['global_fields'])
-        self.event_queue.append(event)
-
+                    event = self.create_event(line, match.groupdict(), event_conf)
+                    self.event_queue.append(event)
+                    return
