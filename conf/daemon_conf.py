147e4dd9024fefacb2c356995aad9409a80a4b0d
Ronald Kaiser <raios.catodicos@gmail.com>
2011-07-19 10:50:03 -0300

    back with conf files, ¬¬

---
+++ conf/daemon_conf.py
@@ -0,0 +1,44 @@
+"""
+    File: daemon_conf.py
+    Description: LogCollector configuration file.
+"""
+
+#PID SETTINGS-----------------------------------------------
+
+#PID_PATH: LogCollector pid path/filename
+PID_PATH="/home/kaiser/workspace/log-collector/collector.pid"
+
+#ACTIVEMQ SETTINGS-----------------------------------------------
+
+#ACTIVEMQ_SERVER: activemq stomp server
+ACTIVEMQ_SERVER="localhost"
+
+#ACTIVEMQ_PORT: activemq stomp port
+ACTIVEMQ_PORT=61613
+
+#APPLICATION LOG SETTINGS-----------------------------------------------
+
+#LOGGING_CONF_FILENAME: log confs for the collector starting process.
+LOGGING_CONF_FILENAME="/home/kaiser/workspace/log-collector/conf/logging.conf"
+
+#LOGGING_PATH: sets where the logs will be located.
+LOGGING_PATH="/home/kaiser/workspace/log-collector/logs/"
+
+#SEVERITY: changes the level of severity for all application logs created.
+#Options: DEBUG, INFO, WARNING, ERROR, CRITICAL
+SEVERITY="DEBUG"
+
+#FORMATTER: sets the format of application log lines.
+#For a complete reference of options, see:
+#http://docs.python.org/library/logging.html#logrecord-attributes
+FORMATTER="%(asctime)s - %(filename)s (%(lineno)d) [(%(threadName)-10s)] %(levelname)s - %(message)s"
+
+#ROTATING: describes when the log files will be rotated.
+#Options: 
+#'S'	Seconds
+#'M'	Minutes
+#'H'	Hours
+#'D'	Days
+#'W'	Week day (0=Monday)
+#'midnight'	Roll over at midnight
+ROTATING="midnight"
---
+++ conf/pattern_conf.py
@@ -0,0 +1,27 @@
+"""
+    File: pattern_conf.py
+    Description: example of log configuration.
+"""
+
+conf = [{
+           'log_filename': '/home/kaiser/oi_vendas_online.access.log',
+           'global_fields': { 
+               'host': 'WebServer-VendasOnline',
+               'log_type': 'apache-access-log',
+           },
+           'events_conf': [{
+                'eventtype': 'VendasOnline',
+                'regexps': ['^.*GET /sale/wizardControl.html?prd_code1=9052.*$'],
+                'consolidation_conf': {
+                    'period' : 1,
+                    'enable': True,
+                    'field': 'acessos',
+                    'user_defined_fields': {
+                        'provedor': 'Oi'}
+                },
+                'one_event_per_line_conf': {
+                    'user_defined_fields': {
+                        'provedor': 'Oi'}
+                }
+            }],
+}]
