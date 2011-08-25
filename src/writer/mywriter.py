405e645c09d25827c6b6528b4ab26f7d44dd218c
Ronald Kaiser <raios.catodicos@gmail.com>
2011-08-25 19:50:16 -0300

    activemq writer

--- src/writer/mywriter.py
+++ src/writer/mywriter.py
@@ -1,9 +1,25 @@
-import sys; sys.path.append("../")
+import time
+import json
 
 from writer import *
+from lib import stomp_sender
 
 
-class MyWriter(Writer):
+class MyWriter(Writer): #activemqwriter
     def write(self, msg):
-        print "\n[WRITER] message: %s" % msg
-
+        #TODO: do exception handling
+        headers = { 'destination': '/queue/events',
+                    'timestamp': int(time.time())*1000}
+        body = {'message' : msg}
+        body = json.dumps(body)
+ 
+        print headers
+        print body
+        try:
+            stomp_sender.send_message_via_stomp([('localhost', 61613)], headers, body)
+            print "sent!"
+            return True
+        except Exception, e:
+            print e
+            print "can't send"
+            return False
