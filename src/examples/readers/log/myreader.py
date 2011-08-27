03cf8ac1509ca68f4a70abee25d6928ceecee5b3
Ronald Kaiser <raios.catodicos@gmail.com>
2011-08-27 18:09:00 -0300

    adding log tail reader

---
+++ src/examples/readers/log/myreader.py
@@ -0,0 +1,14 @@
+import filetail
+
+from __reader import *
+
+
+class MyReader(Reader):
+    def setup(self):
+        self.periodic = False
+        self.tail = filetail.Tail("/tmp/access.log", max_sleep=1)
+
+    def read(self):
+        while True:
+            line = self.tail.nextline()
+            self.store(line)
