e1be570d2be26894f6bbdcd08627fff5b8f37316
Ronald Kaiser <raios.catodicos@gmail.com>
2011-12-17 20:59:03 -0200

    refactoring + commenting __reader

--- src/__reader.py
+++ src/__reader.py
@@ -6,17 +6,14 @@ import helpers.kronos as kronos
 
 
 class Reader(threading.Thread):
-    def __init__(self, queue, writer=None, interval=None):
-        self.writer = writer
-        self.interval = interval
+    def __init__(self, queue, conf=None, writer=None, interval=None):
         self.queue = queue
+        self.interval = interval
+        self.writer = writer
         self.setup()
+
         self.schedule_tasks()
         threading.Thread.__init__(self)
-
-    def reschedule_tasks(self):
-        self.schedule_tasks()
-        self.scheduler.start()
 
     def schedule_tasks(self):
         self.scheduler = kronos.ThreadedScheduler()
@@ -26,7 +23,7 @@ class Reader(threading.Thread):
             self.schedule_single_task()
 
     def schedule_interval_task(self):
-        self.scheduler.add_interval_task(self.process,
+        self.scheduler.add_interval_task(self._process,
                                          "periodic task",
                                          0,
                                          self.interval,
@@ -35,33 +32,42 @@ class Reader(threading.Thread):
                                          None)
 
     def schedule_single_task(self):
-        self.scheduler.add_single_task(self.process,
+        self.scheduler.add_single_task(self._process,
                                        "single task",
                                        0,
                                        kronos.method.threaded,
                                        [],
                                        None)
 
-    def __writer_callback(self):
-        """Callback to writer for non periodic tasks"""
+    def _writer_callback(self):
+        """Callback to writer for non periodic tasks.
+           Shouldn't be called by subclasses."""
         if self.writer and not self.writer.interval:
             self.writer.process()
 
     def _store(self, msg):
+        """Internal method to store read messages.
+           Shouldn't be called by subclasses."""
         if self.queue.qsize() < self.queue.maxsize:
             self.queue.put(msg)
-            self.__writer_callback()
+            self._writer_callback()
         else:
             print "discarding message [%s], full queue" % msg
 
-    def process(self):
+    def _process(self):
+        """Method called internally to process (read) a message.
+           It is called in the end of each interval 
+           in the case of a periodic task.
+           Shouldn't be called by subclasses"""
         msg = self._read()
-        if not msg and self.interval:
+        if not msg:
             print "discarding message due to an error"
-        elif self.interval:
+        elif msg and self.interval:
             self.store(msg)
 
     def _read(self):
+        """Internal method that calls read() method. 
+           Shouldn't be called by subclasses."""
         try:
             return self.read()
         except Exception, e:
@@ -70,6 +76,8 @@ class Reader(threading.Thread):
             return False
 
     def store(self, msg):
+        """Stores a read message. 
+           This may be called by subclasses."""
         try:
             self._store(msg)
         except Exception, e:
@@ -86,5 +94,6 @@ class Reader(threading.Thread):
         pass
 
     def run(self):
+        """Starts the reader"""
         self.scheduler.start()
 
