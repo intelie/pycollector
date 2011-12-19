d2169b84a004c354b34de0a20fee3d5c90fa9693
Ronald Kaiser <raios.catodicos@gmail.com>
2011-12-18 23:54:33 -0200

    iproving exception treatment for ActivemqWriter

--- src/rwtypes/writers/activemq/ActivemqWriter.py
+++ src/rwtypes/writers/activemq/ActivemqWriter.py
@@ -8,10 +8,11 @@ from __writer import *
 
 class ActivemqWriter(Writer):
     def write(self, msg):        
-        headers = {'destination' : self.destination,
-                   'timestamp' : int(time.time()*1000)}
-        body = json.dumps(msg)
         try:
+            headers = {'destination' : self.destination,
+                       'timestamp' : int(time.time()*1000)}
+            body = json.dumps(msg)
+
             stomp_sender.send_message_via_stomp([(self.host, self.port)], headers, body)
             return True
         except Exception, e:
