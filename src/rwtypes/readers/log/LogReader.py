060ec1f366893f0dfca143941d52cb2862c499cb
Ronald Kaiser <raios.catodicos@gmail.com>
2011-12-19 19:06:09 -0200

    delimiters for log reader (wip)

--- src/rwtypes/readers/log/LogReader.py
+++ src/rwtypes/readers/log/LogReader.py
@@ -9,7 +9,20 @@ class LogReader(Reader):
         
     def read(self):
         while True:
-            line  = self.tail.nextline()
-            self.store(line)
+            line = self.tail.nextline()
 
+            #XXX: remove \n?
+            line = line.strip()
+
+            if hasattr(self, 'delimiter'):
+                values = line.split(self.delimiter)
+
+                if hasattr(self, 'columns'):
+                    column_values = dict(zip(self.columns, values))
+                    #XXX: if we have columns, save it as a dict
+                    self.store(column_values)
+                    continue
+
+                #XXX: if we have just a delimiter, save the list
+                self.store(values)
 
