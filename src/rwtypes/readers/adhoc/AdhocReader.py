8443e9d25b82c7e8e72856faaa4eaf196cdeb83f
Ronald Kaiser <raios.catodicos@gmail.com>
2011-12-29 15:47:14 -0200

    adhoc test for sending message in syslog format

--- src/rwtypes/readers/adhoc/AdhocReader.py
+++ src/rwtypes/readers/adhoc/AdhocReader.py
@@ -1,11 +1,17 @@
+import time
 import os
+import socket
 
 from __reader import Reader
 
 class AdhocReader(Reader):
     def read(self):
         try:
-            self.store(os.popen('acpi').read())   
+            date = time.strftime("%b %d %H:%M:%S")
+            msg = "<14>%s %s pycollector: %s" % (date, socket.gethostname(), os.popen('acpi').read())
+            self.store(msg)
+            return True
         except Exception, e:
             print 'error reading'
             print e
+            return False
