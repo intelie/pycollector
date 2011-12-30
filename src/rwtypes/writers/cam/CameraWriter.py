a03e2fb5834cc3c812894a1c1ff89397fa2855ff
Ronald Kaiser <raios.catodicos@gmail.com>
2011-12-30 21:01:42 -0200

    reading path from conf

--- src/rwtypes/writers/cam/CameraWriter.py
+++ src/rwtypes/writers/cam/CameraWriter.py
@@ -4,9 +4,15 @@ from __writer import Writer
 
 
 class CameraWriter(Writer):
+    def setup(self):
+        if not hasattr(self, 'path'):
+            self.path = '/tmp'
+        elif self.path[-1] == '/':
+            self.path = self.path[:-1]
+
     def write(self, msg):
         try:
-            msg.save('/tmp/%s.jpg' % time.time())
+            msg.save('%s/%s-%s.jpg' % (self.path, self.prefix, time.time()))
             return True
         except Exception, e:
             print 'error writing img'
