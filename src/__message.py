b8e7e98b155226e5b55a4a5f509af6b484cd094b
Ronald Kaiser <raios.catodicos@gmail.com>
2012-01-05 12:44:48 -0200

    adding __str__ to message object

--- src/__message.py
+++ src/__message.py
@@ -3,3 +3,6 @@ class Message:
     def __init__(self, content=None, checkpoint=None):
         self.checkpoint = checkpoint
         self.content = content
+
+    def __str__(self):
+        return str(self.__dict__)
