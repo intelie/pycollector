1328a83eb21e32e373b77226afcbb134e540707a
Ronald Kaiser <raios.catodicos@gmail.com>
2012-02-04 23:11:54 -0200

    documenting __message.py

--- src/__message.py
+++ src/__message.py
@@ -1,4 +1,10 @@
 
+"""
+    File: __message.py
+    Description: Implementation of what should be put in the 
+                 internal queue (see architecture in README).
+"""
+
 class Message:
     def __init__(self, content=None, checkpoint=None):
         self.checkpoint = checkpoint
