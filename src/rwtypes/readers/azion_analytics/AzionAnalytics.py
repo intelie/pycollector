8ad29f35f5a0b74b8a8ba07fce04ab8b5439171d
Ronald Kaiser <raios.catodicos@gmail.com>
2012-02-06 10:01:46 -0200

    get nginx clientid

--- src/rwtypes/readers/azion_analytics/AzionAnalytics.py
+++ src/rwtypes/readers/azion_analytics/AzionAnalytics.py
@@ -8,13 +8,14 @@ class AzionAnalytics(Reader):
     """Conf:
         - logpath (required): path of log file,
             e.g. /tmp/my.log
-        - delimiter (optional): character to split log lines,
+        - delimiter (required): character to split log lines,
             e.g. '\t'
-        - columns (optional): list of columns for each log line,
+        - columns (required): list of columns for each log line,
             e.g. ['date', 'hour', 'message']"""
 
     def setup(self):
         self.tail = filetail.Tail(self.logpath, max_sleep=1)
+        self.client = logpath.split('.')[1] 
         
     def read(self):
         while True:
@@ -29,13 +30,6 @@ class AzionAnalytics(Reader):
 
                         #XXX: if we have columns, save it as a dict
                         self.store(Message(content=column_values))
-                        continue
-
-                    #XXX: if we have just a delimiter, save the list
-                    self.store(Message(content=values))
-                    continue
-
-                self.store(Message(content=line))
 
             except Exception, e:
                 print 'error reading line: %s' % line
