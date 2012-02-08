6c4736aad092d3c48a76d835becb1f5b5cc2251f
Ronald Kaiser <raios.catodicos@gmail.com>
2012-02-08 16:06:09 -0200

    renaming conf properties

--- conf/daemon_conf.py
+++ conf/daemon_conf.py
@@ -7,25 +7,25 @@
 #------------
 
 #PID_PATH: LogCollector pid path/filename
-#PID_PATH="../pycollector.pid"
+#PID_FILE_PATH="/your_path/pycollector.pid"
 
 
 #APPLICATION LOG SETTINGS
 #------------------------
 
-#LOGGING_PATH: sets where the logs will be located.
-#LOGGING_PATH="../logs/"
+#LOG_FILE_PATH: sets where the logs will be located.
+#LOG_FILE_PATH="/your_path/logs/"
 
-#SEVERITY: changes the level of severity for all application logs created.
+#LOG_SEVERITY: changes the level of severity for all application logs created.
 #Options: DEBUG, INFO, WARNING, ERROR, CRITICAL
-#SEVERITY="DEBUG"
+#LOG_SEVERITY="DEBUG"
 
-#FORMATTER: sets the format of application log lines.
+#LOG_FORMATTER: sets the format of application log lines.
 #For a complete reference of options, see:
 #http://docs.python.org/library/logging.html#logrecord-attributes
-#FORMATTER="%(asctime)s - %(filename)s (%(lineno)d) [(%(threadName)-10s)] %(levelname)s - %(message)s"
+#LOG_FORMATTER="%(asctime)s - %(filename)s (%(lineno)d) [(%(threadName)-10s)] %(levelname)s - %(message)s"
 
-#ROTATING: describes when the log files will be rotated.
+#LOG_ROTATING: describes when the log files will be rotated.
 #Options: 
 #'S'	Seconds
 #'M'	Minutes
@@ -33,4 +33,4 @@
 #'D'	Days
 #'W'	Week day (0=Monday)
 #'midnight'	Roll over at midnight
-#ROTATING="midnight"
+#LOG_ROTATING="midnight"
