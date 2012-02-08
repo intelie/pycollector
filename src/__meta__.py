41e9efe1d1054042476097dc48eac1efe48647f6
Ronald Kaiser <raios.catodicos@gmail.com>
2012-02-08 15:25:00 -0200

    centralizing paths and default values

---
+++ src/__meta__.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+try:
+    BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    PATHS = {
+        'BASE_PATH' : BASE_PATH,
+        'CONF_PATH' : os.path.join(BASE_PATH, "conf"),
+        'HELPERS_PATH' : os.path.join(BASE_PATH, "src", "helpers"),
+        'THIRD_PATH' : os.path.join(BASE_PATH, "src", "third"),
+        'RWTYPES_PATH' : os.path.join(BASE_PATH, "src", "rwtypes"),}
+    DEFAULTS = {
+        'LOG_FILEPATH' : os.path.join(BASE_PATH, "logs", "pycollector.log"),
+        'PID_FILEPATH': os.path.join(BASE_PATH, "pycollector.pid"),
+        'LOG_SEVERITY' : "DEBUG",
+        'LOG_ROTATING' : 'midnight',
+        'LOG_FORMATTER' : '%(asctime)s - %(filename)s (%(lineno)d) [(%(threadName)-10s)] % (levelname)s - %(message)s',}
+except Exception, e:
+    print e
