bacbb21e622874cd22f3882c24f97cf516c8e3bc
Ronald Kaisera <raios.catodicos@gmail.com>
2012-02-27 00:38:19 -0300

    guarantees that callback does not block new puts in the queue

--- src/__queue.py
+++ src/__queue.py
@@ -3,6 +3,7 @@
     Description: Customized Queue based on Queue builtin module
 """
 
+import time
 import Queue
 import thread
 
@@ -11,12 +12,24 @@ class CustomQueue(Queue.Queue):
     def __init__(self, maxsize=0, callback=None):
         Queue.Queue.__init__(self, maxsize)
         self.callback = callback
+        self.n = 0
+        self.deliverer_running = False
+
+    def deliverer(self):
+        if not self.callback is None:
+            while self.n > 0:
+                self.callback()
+                self.n -= 1
+        self.deliverer_running = False
 
     def put(self, item, block=True, timeout=None):
         Queue.Queue.put(self, item, block, timeout)
 
-        if not self.callback is None:
-            thread.start_new_thread(self.callback, (item,))
+        self.n += 1
+
+        if not self.deliverer_running:
+            self.deliverer_running = True
+            thread.start_new_thread(self.deliverer, ())
 
     def __str__(self):
         return "%s/%s" % (self.qsize(), self.maxsize)
---
+++ src/tests/test_queue.py
@@ -0,0 +1,57 @@
+import random
+import time
+import unittest
+
+import sys; sys.path.append('..')
+from __queue import CustomQueue
+
+
+class TestQueue(unittest.TestCase):
+    def test_callback_should_not_interfere_on_processing_messages_order(self):
+        processed = []
+
+        def callback():
+            item = cq.get()
+            #simulates random processing time
+            time.sleep(int(random.random()*5))
+            processed.append(item)
+
+        cq = CustomQueue(2<<3, callback)
+
+        #adds messages to queue
+        for i in range(2<<2):
+            cq.put(i)
+
+        #waits processing
+        while len(processed) < 2<<2:
+            time.sleep(1)
+
+        # guarantees order of processed msgs
+        self.assertEqual(range(0, 8), processed)
+
+
+    def test_callback_should_not_interfere_on_new_puts(self):
+        def callback():
+            cq.get()
+            cq.task_done()
+            #delays processing
+            time.sleep(1000)
+
+        cq = CustomQueue(2<<3, callback)
+
+        #adds messages to queue
+        for i in range(2<<3):
+            cq.put(i)
+
+        #waits to first callback
+        time.sleep(1)
+
+        # assert all other messages were put in
+        # the queue while the first is being processed
+        self.assertEqual(15, cq.qsize())
+
+
+
+if __name__ == "__main__":
+    unittest.main()
+
