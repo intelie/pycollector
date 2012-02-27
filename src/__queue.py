6a608d10c1ff78b1c5501fc0d6f024a5155214cf
Ronald Kaisera <raios.catodicos@gmail.com>
2012-02-26 22:59:16 -0300

    adding callback to __queue

--- src/__queue.py
+++ src/__queue.py
@@ -4,8 +4,19 @@
 """
 
 import Queue
+import thread
 
 
 class CustomQueue(Queue.Queue):
+    def __init__(self, maxsize=0, callback=None):
+        Queue.Queue.__init__(self, maxsize)
+        self.callback = callback
+
+    def put(self, item, block=True, timeout=None):
+        Queue.Queue.put(self, item, block, timeout)
+
+        if not self.callback is None:
+            thread.start_new_thread(self.callback, (item,))
+
     def __str__(self):
         return "%s/%s" % (self.qsize(), self.maxsize)
