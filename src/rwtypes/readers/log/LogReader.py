9d90f420dfce67621e83fd4ae39e631345273c8c
Ronald Kaiser <raios.catodicos@gmail.com>
2012-02-29 12:21:29 -0300

    refactoring LogReader to accept async or sync confs

--- src/rwtypes/readers/log/LogReader.py
+++ src/rwtypes/readers/log/LogReader.py
@@ -17,35 +17,36 @@ class LogReader(Reader):
     def setup(self):
         self.log = logging.getLogger()
         self.tail = filetail.Tail(self.logpath, max_sleep=1)
-        
+
+        #TODO: implement checkpoints
+
+        #TODO: set with log line number
+        self.checkpoint = 0 
+
+    def process_line(self):
+        try:
+            to_store = self.tail.nextline()
+
+            if hasattr(self, 'delimiter'):
+                to_store = to_store.strip().split(self.delimiter)
+
+                if hasattr(self, 'columns'):
+                    to_store = dict(zip(self.columns, to_store))
+
+            if self.store(Message(content=to_store,
+                               checkpoint=self.checkpoint)):
+
+                #TODO: update with log line number
+                self.checkpoint += 1
+            return True
+
+        except Exception, e:
+            self.log.error('error reading line: %s' % line)
+            return False
+    
     def read(self):
-        checkpoint = 0 
-        while True:
-            try:
-                line = self.tail.nextline()
-
-                if hasattr(self, 'delimiter'):
-                    values = line.strip().split(self.delimiter)
-
-                    if hasattr(self, 'columns'):
-                        column_values = dict(zip(self.columns, values))
-
-                        #XXX: if we have columns, save it as a dict
-                        checkpoint += 1
-                        self.store(Message(content=column_values,
-                                           checkpoint=checkpoint))
-                        continue
-
-                    #XXX: if we have just a delimiter, save the list
-                    self.store(Message(content=values,
-                                       checkpoint=checkpoint))
-                    checkpoint += 1
-                    continue
-
-                self.store(Message(content=line,
-                                   checkpoint=checkpoint))
-                checkpoint += 1
-
-            except Exception, e:
-                self.log.error('error reading line: %s' % line)
-                self.log.error(e)
+        if self.interval:
+            return self.process_line()
+        else:            
+            while True:
+                self.process_line()
