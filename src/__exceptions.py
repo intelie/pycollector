78c6874961112f434eaef3a3c350012013eea631
Ronald Kaiser <raios.catodicos@gmail.com>
2012-03-12 18:08:40 -0300

    adding __str__ to __exceptions

--- src/__exceptions.py
+++ src/__exceptions.py
@@ -7,6 +7,9 @@ class GenericError(Exception):
     def __init__(self, msg=''):
         self.msg = msg
 
+    def __str__(self):
+        return str(self.msg)
+
 class ConfigurationError(GenericError):
     """Raised when there is something missing or wrong in conf.yaml"""
 
