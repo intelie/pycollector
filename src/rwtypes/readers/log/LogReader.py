d9d4eb8a00252aef4036edd6a23de1965f02c06e
Ronald Kaiser <raios.catodicos@gmail.com>
2012-03-19 14:18:14 -0300

    log reader refactoring

--- src/rwtypes/readers/log/LogReader.py
+++ src/rwtypes/readers/log/LogReader.py
@@ -20,29 +20,6 @@ class LogReader(Reader):
         - columns (optional): list of columns for each log line,
             e.g. ['date', 'hour', 'message']"""
 
-    def get_line(self):
-        """Returns a boolean indicating whether the log line
-           was successfully read or not"""
-        try:
-            self.bytes_read, self.current_line = self.tail.nextline()
-            if self.checkpoint_enabled:
-                self.current_checkpoint['bytes_read'] = self.bytes_read
-
-            if self.to_split and self.to_dictify:
-                self.current_line = LogUtils.dictify_line(self.current_line,
-                                                      self.delimiter,
-                                                      self.columns)
-            elif self.to_split:
-                self.current_line = LogUtils.split_line(self.current_line,
-                                                    self.delimiter)
-        except ParsingError, e:
-            self.log.error(e.msg)
-            return False
-        except Exception, e:
-            self.log.error(traceback.format_exc())
-            return False
-        return True
-
     def set_current_datetime(self):
         """Set datetime from the current line"""
         try:
@@ -83,7 +60,6 @@ class LogReader(Reader):
                     s['remaining'] = {}
                     s['zeros'] = []
                     self.current_sums[i]['value'] += current_value_to_sum
-        self.store_sums()
 
     def store_sums(self):
         for s in self.current_sums:
@@ -144,7 +120,6 @@ class LogReader(Reader):
                     # TODO: apply regexp
                     if current_value == s['column_value']:
                         s['value'] += 1
-        self.store_counts()
 
     def store_counts(self):
         # TODO: merge with store_sums in one function
@@ -174,31 +149,14 @@ class LogReader(Reader):
                 for content in contents:
                     self.store(Message(content=content))
 
-    def process_line(self):
-        try:
-            if self.to_sum or self.to_count:
-                self.set_current_datetime()
-                if self.to_sum: self.do_sums()
-                if self.to_count: self.do_counts()
-            else:
-                if self.checkpoint_enabled:
-                    checkpoint = copy.deepcopy(self.current_checkpoint)
-                    self.store(Message(checkpoint=checkpoint,
-                                       content=self.current_line))
-                else:
-                    self.store(Message(content=self.current_line))
-        except Exception, e:
-            self.log.error("Error processing line")
-            self.log.error(traceback.format_exc())
-
-    def set_checkpoint(self):
-        if self.checkpoint_enabled:
-            self.current_checkpoint = self.last_checkpoint or {}
-            if 'bytes_read' in self.last_checkpoint:
-                self.log.info("Detected checkpoint, seeking file: %s to %s position" %
-                              (self.logpath,
-                               self.last_checkpoint['bytes_read']))
-                self.tail.seek_bytes(self.current_checkpoint['bytes_read'])
+    def recover_checkpoint(self):
+        """Seek file if previous checkpoint was found."""
+        self.current_checkpoint = self.last_checkpoint or {}
+        if 'bytes_read' in self.last_checkpoint:
+            self.log.info("Detected checkpoint, seeking file: %s to %s position" %
+                          (self.logpath,
+                           self.last_checkpoint['bytes_read']))
+            self.tail.seek_bytes(self.current_checkpoint['bytes_read'])
 
     def setup(self):
         # starts the logger
@@ -209,7 +167,7 @@ class LogReader(Reader):
         self.validate_conf()
 
         # failure recovering from checkpoint
-        self.set_checkpoint()
+        if self.checkpoint_enabled: self.recover_checkpoint()
 
         # initializations
         self.to_split = True if hasattr(self, 'delimiter') else False
@@ -226,11 +184,50 @@ class LogReader(Reader):
         # starts tail
         self.tail = filetail.Tail(self.logpath, max_sleep=1, store_pos=True)
 
+    def process_line(self):
+        try:
+            if self.to_sum or self.to_count:
+                self.set_current_datetime()
+                if self.to_sum: self.do_sums() and self.store_sums()
+                if self.to_count: self.do_counts() and self.store_counts()
+                return
+
+            if self.checkpoint_enabled:
+                checkpoint = copy.deepcopy(self.current_checkpoint)
+                self.store(Message(checkpoint=checkpoint,
+                                   content=self.current_line))
+            else:
+                self.store(Message(content=self.current_line))
+        except Exception, e:
+            self.log.error("Error processing line")
+            self.log.error(traceback.format_exc())
+
+    def get_line(self):
+        """Returns a boolean indicating whether the log line
+           was successfully read or not"""
+        try:
+            self.bytes_read, self.current_line = self.tail.nextline()
+            if self.checkpoint_enabled:
+                self.current_checkpoint['bytes_read'] = self.bytes_read
+
+            if self.to_split and self.to_dictify:
+                self.current_line = LogUtils.dictify_line(self.current_line,
+                                                      self.delimiter,
+                                                      self.columns)
+            elif self.to_split:
+                self.current_line = LogUtils.split_line(self.current_line,
+                                                    self.delimiter)
+        except ParsingError, e:
+            self.log.error(e.msg)
+            return False
+        except Exception, e:
+            self.log.error(traceback.format_exc())
+            return False
+        return True
+
     def read(self):
-        # sync reader
         if self.period:
             self.get_line() and self.process_line()
-        # async reader
-        else:
-            while True:
-                self.get_line() and self.process_line()
+            return
+        while True:
+            self.get_line() and self.process_line()
