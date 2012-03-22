3de29438fafef600fd08b3ded2de462801849395
Ronald Kaiser <raios.catodicos@gmail.com>
2012-03-22 17:37:51 -0300

    refactoring

--- src/rwtypes/readers/log/LogConfReader.py
+++ src/rwtypes/readers/log/LogConfReader.py
@@ -8,33 +8,20 @@ class LogConfReader:
     def validate_conf(cls, conf):
         counts = conf.get('counts', [])
         sums = conf.get('sums', [])
-        #XXX: refactor me
-        for sum in sums:
-            if 'groupby' in sum and \
-                not 'match' in sum['groupby']:
+        confs = counts + sums
+        for c in confs:
+            if 'groupby' in c and \
+                not 'match' in c['groupby']:
                 raise ConfigurationError("'groupby' must have a 'match' regexp entry in your conf.")
 
-            if 'groupby' in sum and \
-                not 'column' in sum['groupby']:
+            if 'groupby' in c and \
+                not 'column' in c['groupby']:
                 raise ConfigurationError("'groupby' must have a 'column' entry in your conf.")
 
-            try:
-                if re.compile(sum['groupby']['match']).groups != 1:
-                    raise ConfigurationError("'groupby' must contain a 'match' regexp with exactly one group in your conf.")
-            except Exception, e:
-                raise ConfigurationError("'match' regexp is not valid.")
+            if 'groupby' in c:
+                try:
+                    if re.compile(c['groupby']['match']).groups != 1:
+                        raise ConfigurationError("'groupby' must contain a 'match' regexp with exactly one group in your conf.")
+                except Exception, e:
+                    raise ConfigurationError("'match' regexp: %s is not valid." % c['groupby']['match'])
 
-        for count in counts:
-            if 'groupby' in count and \
-                not 'match' in count['groupby']:
-                raise ConfigurationError("'groupby' must have a 'match' regexp entry in your conf.")
-
-            if 'groupby' in count and \
-                not 'column' in count['groupby']:
-                raise ConfigurationError("'groupby' must have a 'column' entry in your conf.")
-
-            try:
-                if re.compile(count['groupby']['match']).groups != 1:
-                    raise ConfigurationError("'groupby' must contain a 'match' regexp with exactly one group in your conf.")
-            except Exception, e:
-                raise ConfigurationError("'match' regexp is not valid.")
