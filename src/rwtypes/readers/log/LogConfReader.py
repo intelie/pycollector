45c9038bb141b2d7a3cb3d26dc3a7a802d0cc4ee
Ronald Kaiser <raios.catodicos@gmail.com>
2012-03-22 16:44:16 -0300

    making test pass

--- src/rwtypes/readers/log/LogConfReader.py
+++ src/rwtypes/readers/log/LogConfReader.py
@@ -5,5 +5,9 @@ class LogConfReader:
     def validate_conf(cls, conf):
         if 'groupby' in conf and \
             not 'match' in conf['groupby']:
-            raise ConfigurationError("groupby' must have a match regexp in your conf.")
+            raise ConfigurationError("'groupby' must have a 'match' regexp entry in your conf.")
+
+        if 'groupby' in conf and \
+            not 'column' in conf['groupby']:
+            raise ConfigurationError("'groupby' must have a 'column' entry in your conf.")
 
