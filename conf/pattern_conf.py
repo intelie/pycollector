5603a6b44f4d28bb67b176c678860c16064777e0
Juan Lopes <me@juanlopes.net>
2012-07-05 12:57:50 -0300

    better gitignore

--- conf/pattern_conf.py
+++ conf/pattern_conf.py
@@ -11,12 +11,12 @@ conf = [{
            },
            'events_conf': [{
                 'eventtype': 'Welcome',
-                'regexps': ['(?P<test>\\d+:\\d+:\\d+)'],
+                'regexps': ['^(?P<test>.*)$'],
                 'consolidation_conf': {
                     'period' : 1/60.0,
                     'enable': True,
                     'field': 'acessos',
-                    'unique_fields': [['test', 4]],
+                    'unique_fields': [['test', 14]],
                     'user_defined_fields': {
                         'provedor': 'Oi'}
                 }
