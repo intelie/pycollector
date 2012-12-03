68275f341641b102e13e1201e5320131c86782d4
Danillo Souza <danillo012@gmail.com>
2012-12-03 19:06:52 -0200

    TODO: Fix MessageEntity Metaclass Hack (generate table based in object/hash)

--- src/tests/test_db_writer.py
+++ src/tests/test_db_writer.py
@@ -5,11 +5,16 @@ from rwtypes.writers.db.DBWriter import DBWriter
 from rwtypes.writers.db.MessageEntity import MessageEntity
 from third.sqlalchemy import create_engine
 from third.sqlalchemy.orm import sessionmaker
+from dateutil.tz import tzlocal
+import datetime
+import logging
 
 test_messages = [
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-    "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."
+    {'interval_started_at': datetime.datetime(2012, 9, 29, 19, 5, tzinfo=tzlocal()), 'value': 0},
+    {'interval_started_at': datetime.datetime(2012, 7, 19, 19, 5, tzinfo=tzlocal()), 'value': 0},
+    {'interval_started_at': datetime.datetime(2012, 5, 25, 19, 5, tzinfo=tzlocal()), 'value': 0},
+    {'interval_started_at': datetime.datetime(2012, 3, 25, 19, 5, tzinfo=tzlocal()), 'value': 0},
+    {'interval_started_at': datetime.datetime(2012, 1, 10, 19, 5, tzinfo=tzlocal()), 'value': 0}
 ]
 
 def get_queue(maxsize=1024):
@@ -23,28 +28,25 @@ conf = {'database': 'test',
         'connection': 'mysql://%s:%s@%s/%s'}
 
 writer = DBWriter(get_queue(), conf=conf)
+writer.log.addHandler(logging.StreamHandler())
 
 for message in test_messages:
     writer.write(message)
 
 
-engine = create_engine("mysql://%s:%s@%s/%s" % (conf['user'],
-                                                conf['passwd'],
-                                                conf['host'],
-                                                conf['database']),
-                                                echo=False)
+#engine = create_engine(conf['connection'] % (conf['user'],
+#                                                conf['passwd'],
+#                                                conf['host'],
+#                                                conf['database']),
+#                                                echo=False)
 
-Session = sessionmaker(bind=engine)
-session = Session()
+#Session = sessionmaker(bind=engine)
+#session = Session()
 
-get_messages = session.query(MessageEntity).all()
+#get_messages = session.query(writer.message_class).all()
 
-for msg in get_messages:
-    if not msg.message in test_messages:
-        print "FAIL!"
-        sys.exit(1)
-    else:
-        print msg.message
+#for msg in get_messages:
+#    print msg
 
-[session.delete(x) for x in session.query(MessageEntity).all()]
-session.commit()
+#[session.delete(x) for x in session.query(MessageEntity).all()]
+#session.commit()
