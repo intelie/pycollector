977991b3b5e427297a3b7c90171a2b6be6211c15
Danillo Souza <danillo012@gmail.com>
2012-12-03 19:08:20 -0200

    TODO: Fix MessageEntity Metaclass Hack (generate table based in object/hash)

--- src/rwtypes/writers/db/DBWriter.py
+++ src/rwtypes/writers/db/DBWriter.py
@@ -5,26 +5,40 @@ import traceback
 import sys; sys.path.append('..')
 import __meta__; __meta__.load_paths()
 from __writer import Writer
+from datetime import datetime
+from MessageEntity import Base
 from MessageEntity import MessageEntity
-from third.sqlalchemy import create_engine
+from third.sqlalchemy import create_engine, Integer, Float, String, DateTime
 from third.sqlalchemy.orm import sessionmaker
+from third.sqlalchemy.engine.reflection import Inspector
 
 class DBWriter(Writer):
 
     def start_session(self):
-        engine = create_engine("mysql://%s:%s@%s/%s" % (self.user,
+        self.engine = create_engine(self.connection % (self.user,
                                                     self.passwd,
                                                     self.host,
                                                     self.database),
                                                     echo=False)
 
-        Session = sessionmaker(bind=engine)
+        Session = sessionmaker(bind=self.engine)
         self.session = Session()
 
     def do_insert(self, msg):
+
+        # verify if table already exists
+        self.start_session()
+        inspector = Inspector.from_engine(self.engine)
+
+        if not (MessageEntity.__tablename__ in inspector.get_table_names()):
+            Base.metadata.create_all(self.engine)
+
         try:
-            self.start_session()
             obj = MessageEntity(msg)
+
+            print obj
+            return
+
             self.session.add(obj)
             self.session.commit()
         except Exception:
@@ -42,7 +56,7 @@ class DBWriter(Writer):
 
     def write(self, msg):
         try:
-            self.do_insert(str(msg))
+            self.do_insert(msg)
 
             return True
         except Exception:
--- src/rwtypes/writers/db/MessageEntity.py
+++ src/rwtypes/writers/db/MessageEntity.py
@@ -3,7 +3,7 @@
 import sys; sys.path.append('..')
 import __meta__; __meta__.load_paths()
 from third.sqlalchemy.ext.declarative import declarative_base
-from third.sqlalchemy import Column, Integer, String
+from third.sqlalchemy import Column, Float, Integer, String, DateTime
 
 Base = declarative_base()
 
@@ -11,12 +11,26 @@ class MessageEntity(Base):
     __tablename__ = 'message'
 
     id = Column(Integer, primary_key=True)
-    message = Column(String(140))
 
     def __init__(self, msg):
-        self.message = str(msg)
+
+        # metaclass hack - generate table on the fly
+        __type_to_field = {
+                            type(1): Integer,
+                            type(1.0): Float,
+                            type('1'): String,
+                        }
+
+        __property_hash = {str(k): __type_to_field.get(type(v), DateTime) for (k,v) in msg.items()}
+
+        for k,v in __property_hash.items():
+            if v in [Integer, Float, DateTime]:
+                __property_hash[k] = v()
+            if v == String:
+                __property_hash[k] = v(1024)
+
 
     def __str__(self):
-        return u"<Message('%s')>" % self.message
+        return str(self.__dict__)
 
 
